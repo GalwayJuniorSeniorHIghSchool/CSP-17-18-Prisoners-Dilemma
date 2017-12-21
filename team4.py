0d4d11be7e9c0c59c849159e90383440e84acb22
bjacaruso <bjacaruso@galwaycsd.org>
2017-12-21 11:18:24 -0500

    Update team4.py

--- team4.py
+++ team4.py
@@ -11,8 +11,6 @@ strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    if their_history[0]:
-        return 'c'
     if my_score > their_score:
         return 'b'
     if my_score <= their_score:
